93726357c2bd967981619ea894b76e06df2c787a
Jason Heng <jasonjheng@gmail.com>
2022-11-27 18:51:13 -0500

    Hall test

--- hall_test.c
+++ hall_test.c
@@ -46,33 +46,6 @@
 ;**********************************************************************
 */
 
-uns16 ServoSpeedL(int percent){
-    uns16 slope = (SERVO_LEFT_STOP-SERVO_1MS) / 100;
-    return (uns16)(SERVO_LEFT_STOP - slope*percent);
-}
-
-uns16 ServoSpeedR(int percent){
-    uns16 slope = (SERVO_2MS-SERVO_RIGHT_STOP) / 100;
-    return (uns16)((slope*percent) + SERVO_RIGHT_STOP);
-}
-
-void LED_Blink(int time){
-	int i = 0;
-	while(i < time){
-		OnLED
-		LongDelay(3.5);
-		OffLED
-		LongDelay(3.5);
-		i++;
-	}
-}
-
-void LED_on(int time){
-	OnLED
-	LongDelay(time);
-	OffLED
-}
-
 void main(void)
 {
     uns8 analog_value_s;
@@ -85,47 +58,13 @@ void main(void)
     while (1)  // loop forever
     {
         analog_value_h = AnalogConvert(ADC_HALL_EFFECT);  // Get analog value from the Hall effect sensor
-		
-        analog_value_s = AnalogConvert(ADC_IR_SENSOR);  // get analog value from IR sensor diff amp
 
-		if(analog_value_h < 0x66){ // voltage below 2
-			LongDelay(0.1);
-			if(analog_value_h < 0x66){ 
-				SetLeft(ServoSpeedL(0));
-				SetRight(ServoSpeedR(0));
-				LED_on(7);
-			}
+		if(analog_value_h < 0x66){ // voltage above 3
+			OffLED
 		}
-		else if(analog_value_h > 0x99){ // voltage above 3 
-			LongDelay(0.1); // debounce
-			if(analog_value_h > 0x99){
-				SetLeft(ServoSpeedL(0));
-				SetRight(ServoSpeedR(0));
-				LED_Blink(7);
-			} 
+		else if(analog_value_h > 0x99){ // voltage below 2 
+			OnLED
 		}
-		
-		if(analog_value_h > 0x66 && analog_value_h < 0x99)
-			if ( analog_value_s < 0x66 )  // 0x66 is 2V for 10-bit ADC with 2 LSB dropped
-			{
-				//Turn Right
-				SetLeft(ServoSpeedL(100));
-				SetRight(ServoSpeedR(0));
-			}
-			else if ( analog_value_s > 0x99 )  // 0x99 is 3V for 10-bit ADC with 2 LSB dropped
-			{
-				//Turn left
-					SetLeft(ServoSpeedL(0));
-					SetRight(ServoSpeedR(100));
-			}
-			else
-			{
-				// both servos on
-				BothServosOn
-			}
-		}
-		
-       
     }
 }
 
